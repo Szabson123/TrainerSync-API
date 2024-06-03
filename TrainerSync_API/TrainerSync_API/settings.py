8b4b1dbc2271d497478544bad4e947343e383af1
Szabson123 <szymonzaba18@gmail.com>
2024-06-03 08:03:54 +0200

    added rest-framework

--- TrainerSync_API/TrainerSync_API/settings.py
+++ TrainerSync_API/TrainerSync_API/settings.py
@@ -47,6 +47,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'rest_framework',
+    
+    'base',
 ]
 
 ROOT_URLCONF = 'TrainerSync_API.urls'
