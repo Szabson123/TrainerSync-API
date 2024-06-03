cc92987a5c5bf4d14ca86cf27d184a094b9c0bed
Szabson123 <szymonzaba18@gmail.com>
2024-06-03 09:47:42 +0200

    added AbstractUser model

--- TrainerSync_API/TrainerSync_API/settings.py
+++ TrainerSync_API/TrainerSync_API/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+     # Downloaded 
+    'rest_framework',
+    
+    # My apps
+    'base',
+    'rooms',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -47,14 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
-    # Downloaded 
-    'rest_framework',
-    
-    # My apps
-    'base',
-    'rooms',
-    'users',
+
 ]
 
 ROOT_URLCONF = 'TrainerSync_API.urls'
@@ -129,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
--- TrainerSync_API/users/models.py
+++ TrainerSync_API/users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    is_trainer = models.BooleanField(default=False)
+    is_manager = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    
+    def __str__(self):
+        return self.username
+
+    REQUIRED_FIELDS = ['email']
+
+    
