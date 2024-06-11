74f9b65b615ae02145a8687682bba012eff15a53
Szabson123 <szymonzaba18@gmail.com>
2024-06-11 12:38:11 +0200

    added BalanceForActivityClass

--- TrainerSync_API/activity_class/admin.py
+++ TrainerSync_API/activity_class/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(ActivityClass)
+admin.site.register(BalanceForActivityClass)
--- TrainerSync_API/activity_class/models.py
+++ TrainerSync_API/activity_class/models.py
@@ -9,6 +9,23 @@ class ActivityClass(models.Model):
     groups = models.ManyToManyField(Group, related_name='groups_in_activity_class', blank=True)
     users = models.ManyToManyField(CustomUser, related_name='users_in_activity_class', blank=True)
     subusers = models.ManyToManyField(SubUser, related_name='sub_users_in_activity_group', blank=True)
+    cost = models.DecimalField(max_digits=6, decimal_places=2, null=True)
     
     def __str__(self) -> str:
         return self.name
+
+
+class BalanceForActivityClass(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='balance_user')
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='room_in_balance')
+    activity_class = models.ForeignKey(ActivityClass, on_delete=models.SET_NULL, null=True, related_name='activity_in_balance')
+    amount_due = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    paid = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def get_name(self):
+        return self.activity_class.name if self.activity_class else "No Activity Class"
+
+    def __str__(self):
+        return f'{self.user} -- {self.get_name()}'
