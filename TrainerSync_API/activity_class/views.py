806b2ad85c01fc6eb5aa1200184001368348ba25
Szabson123 <szymonzaba18@gmail.com>
2024-06-12 15:30:20 +0200

    added list of peyment users

--- TrainerSync_API/activity_class/views.py
+++ TrainerSync_API/activity_class/views.py
@@ -23,12 +23,42 @@ class ActivityClassViewSet(viewsets.ModelViewSet):
         instance.create_balances()
         return instance
 
-    @action(detail=True, methods=['post'])
+    @action(detail=True, methods=['POST'])
     def create_balances(self, request, pk=None):
         activity_class = self.get_object()
         activity_class.create_balances()
         return Response(status=status.HTTP_200_OK)
     
+    @action(detail=True, methods=['GET'])
+    def payment_users_list(self, request, pk=None):
+        activity_class = self.get_object()
+        balance_users = BalanceForActivityClass.objects.filter(activity_class=activity_class)
+        
+        result = []
+        
+        for balance in balance_users:
+            user_data = None
+            sub_user_data = None
+            
+            if balance.user:
+                user_data = {
+                    'user': balance.user.get_full_name() if balance.user else None,
+                    'amount_due': balance.amount_due,
+                    'amount_paid': balance.amount_paid,  
+                    'paid': balance.paid 
+                }
+                result.append(user_data)
+            elif balance.subuser:     
+                sub_user_data = {             
+                    'subuser': f'{balance.subuser.name} opiekunem jest: {balance.subuser.parent}' if balance.subuser else None,
+                    'amount_due': balance.amount_due,
+                    'amount_paid': balance.amount_paid, 
+                    'paid': balance.paid 
+                }
+                result.append(sub_user_data)
+
+        return Response(result, status=status.HTTP_200_OK)
+        
 
 class BalanceForActivityClassViewSet(viewsets.ModelViewSet):
     queryset = BalanceForActivityClass.objects.all()
