763efbd20a4941e8fe49ac2088e825d16aeb4e2b
DESKTOP-TN3VE9B\szymo <szymonzaba75@gmail.com>
2024-08-09 00:21:57 +0200

    added error on register

--- TrainerSync_API/custom_auth/views.py
+++ TrainerSync_API/custom_auth/views.py
@@ -39,7 +39,7 @@ class RegistrationViewSet(viewsets.ViewSet):
         if serializer.is_valid():
             user = serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'error': 'user with this email already exist'}, status=status.HTTP_400_BAD_REQUEST)
     
 
 class CustomTokenObtainPairView(TokenObtainPairView):
